2b02dbf54c29009ff60796e6d72462782660b8fe
Prathyusha-3 <119865125+Prathyusha-3@users.noreply.github.com>
2022-12-05 19:55:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const Greet =() =>{
+    return(
+        <div>
+          <center>
+            <h1>Hello</h1>
+            <h2>Good</h2>
+            </center>
+        </div>
+    )
+}
+export default Greet
